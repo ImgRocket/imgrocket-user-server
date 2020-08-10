a901a497a421275ad86880d5adafc2123f0a8d41
WCF <1533144693@qq.com>
2020-08-10 15:52:33 +0800

    not tested

--- src/main/java/servlet/ApiCode.kt
+++ src/main/java/servlet/ApiCode.kt
@@ -1,5 +1,6 @@
 package servlet
 
+import conn.MySQLConn
 import enums.Message
 import enums.Status
 import model.Usage
@@ -52,3 +53,49 @@ class StartTrail : HttpServlet() {
         writer.write(this.json())
     }
 }
+
+@WebServlet(urlPatterns = ["/code/new"])
+class NewTask : HttpServlet() {
+    private lateinit var writer: PrintWriter
+    override fun doPost(req: HttpServletRequest, resp: HttpServletResponse) {
+        req.characterEncoding = "UTF-8"
+        resp.contentType = "text/html;charset=UTF-8"
+        writer = resp.writer
+        task(req).write()
+    }
+
+    override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+        doPost(req, resp)
+    }
+
+    private fun task(req: HttpServletRequest): Message<Any> {
+        val fields = req.parameterMap.fields()
+        val uid = fields["uid"]
+        val token = fields["token"]
+        if (uid.isNullOrEmpty() || token.isNullOrEmpty()) {
+            return Message(Status.AE, "参数错误", null)
+        } else {
+            User.checkToken(uid, token).let {
+                return when (it) {
+                    Status.OK -> {
+                        val usage = Usage.query(uid)
+                        if (usage != null && usage.used < usage.all) {
+                            MySQLConn.connection.prepareStatement("update `usage` set used = used + 1 where user = ?").apply { setString(1, uid) }.use { ps -> ps.executeQuery() }
+                            Message(Status.OK, "剩余次数足够并-1")
+                        } else {
+                            Message(Status.PME, "无剩余次数，请充值")
+                        }
+                    }
+                    Status.TE -> Message(Status.TE, "TOKEN失效")
+                    Status.UNE -> Message(Status.AIF, "账号不存在")
+                    else -> Message(it, "其他错误")
+                }
+            }
+        }
+    }
+
+    private fun <T> Message<T>.write() {
+        writer.write(this.json())
+    }
+}
+
